f1d6894dded3bee85152fd83a75fba9798d71532
Gabriel <gabrielloures21@gmail.com>
2022-10-11 10:32:03 -0300

    End Details Page

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -9,7 +9,13 @@ export const Container = styled.div`
   grid-template-rows: 105px auto; // primaira linha (row), que é o header, terá 105px de altura fixa e a outra linha, que é o corpo da página, terá altura automática
   grid-template-areas:
     "header"
-    "content"
+    "content";
+
+  > main {
+    grid-area: content;
+    overflow-y: scroll; // quando o conteúdo não couber mais na vertical, apareça uma barra pra scroll
+    padding: 64px 0;
+  }
   
 `;
 
@@ -25,4 +31,30 @@ export const Links = styled.ul`
     }
   }
 
-`
+`;
+
+export const Content = styled.div`
+
+  max-width: 550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    font-size: 36px;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 16px;
+    text-align: justify; // deixa o texto alinhado com as bordas da box certinhos
+  }
+
+`;
