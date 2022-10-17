9f498d5deb138c281651c8dbe03b9039a02aed8c
Gabriel <gabrielloures21@gmail.com>
2022-10-17 10:40:52 -0300

    End Routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
-import { Details } from './pages/Details'
-import { Home } from './pages/Home'
-import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
-import { Profile } from './pages/Profile'
-import { New } from './pages/New'
+
+import { Routes } from './routes'
 
 import theme from './styles/theme';
 
@@ -15,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
