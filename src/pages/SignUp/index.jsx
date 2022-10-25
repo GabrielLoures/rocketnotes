1a9c56d13c546521b070c0e8e964c0a0a79afb64
Gabriel <gabrielloures21@gmail.com>
2022-10-25 09:18:10 -0300

    End Cadastro Usuários

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,17 +1,45 @@
+import { useState } from "react";
 import { Container, Form, Background } from './styles';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 export function SignUp() {
+  
+  const [name, setName] = useState(""); // entre os [] colocamos qual estado será alterado, seguido pela função que irá fazer atualizar esse estado (manter o padrão setNomeDoEstado); entre os () do useState(), colocamos o estado inicial dessa variável, que no caso é vazio
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    
+    if(!name || !email || !password) {
+      alert("Preencha todos os campos!")
+    }
+
+    api.post("/users", { name, email, password })
+    .then(() => {
+      alert("Usuário cadastrado com sucesso!")
+      navigate("/"); // usamos o navigate para levar o usuário diretamente para a página de login logo após um cadastro feito com sucesso
+    })
+    .catch(error => {
+      if(error.response) {
+        alert(error.response.data.message) // pega a mensagem do próprio back-end, lá do arquivo UsersController (AppError)
+      } else {
+        alert("Não foi possível cadastrar!")
+      }
+    })
+
+  }
 
   return (
 
     <Container>
-
       
       <Background></Background>
 
@@ -22,11 +50,28 @@ export function SignUp() {
 
         <h2>Crie sua Conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser}/>
-        <Input placeholder="E-mail" type="text" icon={FiMail}/>
-        <Input placeholder="Senha" type="password" icon={FiLock}/>
+        <Input 
+          placeholder="Nome" 
+          type="text" 
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Input 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <Input 
+          placeholder="Senha" 
+          type="password" 
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <Button title="Cadastrar" onClick={ handleSignUp } />
 
         <Link to="/">
           Voltar para o Login
